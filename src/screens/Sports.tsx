6e04c4f5f885ca2c585191658de446f8495ac828
blazej-bryla <brykuski@gmail.com>
2022-06-07 18:36:05 +0200

    Sports page main design

--- src/screens/Sports.tsx
+++ src/screens/Sports.tsx
@@ -3,11 +3,15 @@ import { SportsType, SportType } from "../types/sports.types";
 import { NoResults } from "../components/NoResults/NoResults";
 import { TableColumn } from "../components/Table/Table";
 import { Visibility } from "@mui/icons-material";
-import {getSports} from "../service/sports.service";
+import { getSports } from "../service/sports.service";
+import {Box, Button, Typography} from "@mui/material";
+import {darkTheme} from "../theme";
 
 export const SportsScreen = () => {
   const [sports, setSports] = useState<SportsType | undefined>(undefined);
-  const [sportDetails, setSportDetails] = useState<SportType | undefined>(undefined);
+  const [sportDetails, setSportDetails] = useState<SportType | undefined>(
+    undefined
+  );
 
   const columns: TableColumn<SportType>[] = [
     { id: "sport", label: "Sport", value: "name" },
@@ -21,23 +25,65 @@ export const SportsScreen = () => {
     },
   ];
 
-  const getSportDetails = (id: SportType['id']) => {
+  const getSportDetails = (id: SportType["id"]) => {
     // TODO: get sport details
-  }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       return await getSports();
     };
     fetchData()
-        .then((r) => setSports(r))
-        .catch(console.error);
+      .then((r) => setSports(r))
+      .catch(console.error);
   }, [setSports]);
-console.log(sports)
+  console.log(sports);
   if (!sports) {
     return <NoResults />;
   }
 
   // TODO: display data got form service
-  return <div>asd</div>;
+  return (
+    <Box
+      sx={{
+        padding: 4,
+      }}
+    >
+      <Typography sx={{
+        fontSize: 24,
+        fontWeight: 550,
+      }}>
+      Sports
+      </Typography>
+      <Typography sx={{
+        paddingTop: 4,
+        paddingBottom: 6
+      }}>
+        {sports.teaser}
+      </Typography>
+      <Box sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        background: darkTheme.palette.background.default,
+        paddingX: 4,
+        paddingY:2,
+      }}>
+      <Typography sx={{
+        color: darkTheme.palette.text.primary,
+        fontSize: 20
+      }}>
+        Sports
+      </Typography>
+        <Button sx={{
+          fontSize: 20,
+          textTransform: "uppercase",
+          backgroundColor: darkTheme.palette.primary.main,
+          color: darkTheme.palette.text.primary,
+          paddingX: 2
+        }}>ADD SPORT</Button>
+      </Box>
+    </Box>
+  );
 };
